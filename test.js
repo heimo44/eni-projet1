7919000f7bd3b874c0d4f8089f10434665429182
heimo44 <heimo.chuong@gmail.com>
2024-07-03 02:25:20 +0200

    preparation du theme uniquement sur le css

---
+++ test.js
@@ -0,0 +1,8 @@
+// export function test(){fetch('apprenants.json')
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(getApprenants(data))
+// })
+// .catch((error) => {
+//     console.error('Une erreur est survenue lors de la recuperation du fichier JSON', error)
+// })}
